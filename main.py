02c092c8130a83b8affab47cf534a214b1fcab78
juri0327 <8721089@ed.tus.ac.jp>
2024-10-24 17:14:15 +0900

    課題完了

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 import numpy as np
 
 def quadratic(A, x):
-    pass
+    A_inv = np.linalg.inv(A)
+    return np.sqrt(np.dot(x.T, np.dot(A_inv, x)))
 
 def second_eig(A):
-    pass
+    eigenvalues =np.linalg.engvalsh(A)
+    return np.sort(eigenvalues)
 
 def power_iter(A, x, k):
-    pass
+    Akx = np.linalg.matrix_power(A, k).dot(x)
+    v1 = Akx / np.linalg.norm(Akx)
+    lambda_1 = np.dot(v1.T, np.dot(A, v1))
+    return lambda_1
